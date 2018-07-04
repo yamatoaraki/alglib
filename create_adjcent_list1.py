af6b63b04801b749041b51d87833259726fe8cbb
NieR Replicant <nierreplicant@NieRnoMac-mini.local>
2018-07-05 07:17:51 +0900

    Competitive programming library

---
+++ create_adjcent_list1.py
@@ -0,0 +1,18 @@
+############この関数はノードとエッジが与えられた場合のみ使える###########
+n = int(input())
+#nはノード数
+p = []
+for i in range(n-1):
+    p.append(list(map(int, input().split())))
+#p=[[3, 6], [1, 2], [3, 1], [7, 4], [5, 7], [1, 4]]
+#pは上のようなエッジのリスト。３と６をつなぐ、１と２をつなぐ・・・
+
+#この関数はノードの数nとエッジのリストを引数にとる。
+def create_adjcect_list(n,p):
+    l = [[] for i in range(n+1)]
+    for pi in p:  #0-originedに注意
+        l[pi[0]].append(pi[1])
+        l[pi[1]].append(pi[0])
+    l.pop(0)#リストの先頭削除
+    print(l)
+create_adjcect_list(n,p)
